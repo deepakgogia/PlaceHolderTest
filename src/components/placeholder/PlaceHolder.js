82e497988bbaa4139114c2b7985810bac12daafb
deepakgogia <48691995+deepakgogia@users.noreply.github.com>
2021-05-09 05:38:02 +0530

    Add files via upload

--- src/components/placeholder/PlaceHolder.js
+++ src/components/placeholder/PlaceHolder.js
@@ -1,1 +1,17 @@
+import './PlaceHolder.css';
+const PlaceHolder = () => {
+    return(
+        <div className='div'>
+            <div className='mainContent' />
+            <div className='otherContent'>
+                <div className='circle'/>
+                <div className='headingDiv'>
+                    <div className='heading1'/>
+                    <div className='heading2'/>
+                </div>
+            </div>
+        </div>
+    )
+}
 
+export default PlaceHolder;
